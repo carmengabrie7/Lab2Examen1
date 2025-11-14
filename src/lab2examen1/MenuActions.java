c3d1f179881534ff4db896ee76fac48ec7b69ad5
Gabriel Zelaya <gabrielzelaya0206@gmail.com>
2025-11-14 08:41:30 -0600

    interface MenuActions

---
+++ src/lab2examen1/MenuActions.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2examen1;
+
+/**
+ *
+ * @author Gabriel
+ */
+public interface MenuActions {
+    void submenu();
+    void ejecutarOpcion(int opcion);
+}
+
